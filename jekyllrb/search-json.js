3d5139ff8111c322461495c29c45c2e498831311
Cristian Sulea <cristian.sulea.79@gmail.com>
2018-10-15 09:34:39 +0300

    search -> "cats_tags_cats": "{{ post.categories | join: ' ' }} {{ post.tags | join: ' ' }} {{ post.categories | join: ' ' }}",

--- jekyllrb/search-json.js
+++ jekyllrb/search-json.js
@@ -7,10 +7,11 @@ layout: null
 [
   {% for post in site.posts %}
     {
-      "title"      : "{{ post.title | escape }}",
-      "categories" : "{{ post.categories | join: ', ' }}",
-      "tags"       : "{{ post.tags | join: ', ' }}",
-      "url"        : "{{ site.baseurl }}{{ post.url }}"
+      "title"         : "{{ post.title | escape }}",
+      "categories"    : "{{ post.categories | join: ' ' }}",
+      "tags"          : "{{ post.tags | join: ' ' }}",
+      "cats_tags_cats": "{{ post.categories | join: ' ' }} {{ post.tags | join: ' ' }} {{ post.categories | join: ' ' }}",
+      "url"           : "{{ site.baseurl }}{{ post.url }}"
     } {% unless forloop.last %},{% endunless %}
   {% endfor %}
 ]
