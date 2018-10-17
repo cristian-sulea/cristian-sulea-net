ffc98b7f50ef14f02a53c1a3987f1d76ba74f33f
Cristian Sulea <cristian.sulea.79@gmail.com>
2018-10-17 11:47:13 +0300

    search.json

--- jekyllrb/assets/simple-jekyll-search-init.js
+++ jekyllrb/assets/simple-jekyll-search-init.js
@@ -4,6 +4,5 @@ SimpleJekyllSearch({
 	searchInput: document.getElementById('search-input'),
 	resultsContainer: document.getElementById('results-container'),
 	searchResultTemplate: '<li><span class="post-meta">{date}</span><h2><a class="post-link" href="{url}">{title}</a></h2><p>{description}</p></li>',
-	//json: '/search-json.min.js'
-	json: '/search-json.js'
+	json: '/search.json'
 })
