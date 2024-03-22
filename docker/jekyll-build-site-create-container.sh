360e5fe4ef75f5fe4ad9d49fbc4ee631994c2249
Cristian Sulea <cristian.sulea.79@gmail.com>
2024-03-22 15:53:50 +0200

    more/better docker helper scripts

--- docker/jekyll-build-site-create-container.sh
+++ docker/jekyll-build-site-create-container.sh
@@ -3,5 +3,5 @@ cd ..
 ProjectFolder=${PWD##*/}          # to assign to a variable
 ProjectFolder=${ProjectFolder:-/} # to correct for the case where PWD=/
 cd site
-docker run -d --name ${ProjectFolder} -v ${PWD}:/site cristiansulea/jekyll-build:1.0
+docker run -d --name ${ProjectFolder}-build -v ${PWD}:/site cristiansulea/jekyll-build:1.0
 #read -p "Press ENTER to continue..."
