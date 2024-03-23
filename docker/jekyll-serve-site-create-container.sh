869dd08f5a9940163f3ea054bcef3877bab89903
Cristian Sulea <cristian.sulea.79@gmail.com>
2024-03-23 19:57:42 +0200

    4001

--- docker/jekyll-serve-site-create-container.sh
+++ docker/jekyll-serve-site-create-container.sh
@@ -3,5 +3,5 @@ cd ..
 ProjectFolder=${PWD##*/}          # to assign to a variable
 ProjectFolder=${ProjectFolder:-/} # to correct for the case where PWD=/
 cd site
-docker run -d --name ${ProjectFolder}-build -v ${PWD}:/site -p 4000:4000 cristiansulea/jekyll-serve:1.0
+docker run -d --name ${ProjectFolder}-serve -v ${PWD}:/site -p 4001:4000 cristiansulea/jekyll-serve:1.0
 #read -p "Press ENTER to continue..."
